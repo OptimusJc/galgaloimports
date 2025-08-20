a4fa2eee3efe52c570ca633dc905ee4dc4ff7e44
OptimusJc <optimus.prime.jc@gmail.com>
2025-08-20 23:10:19 +0300

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  content: ["./node_modules/@relume_io/relume-ui/dist/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@relume_io/relume-ui/dist/**/*.{js,ts,jsx,tsx}",
+  ],
   presets: [require("@relume_io/relume-tailwind")],
 
   darkMode: false, // or 'media' or 'class'
