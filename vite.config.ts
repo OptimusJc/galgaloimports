980b08aab180e1ec272db791f06ffaf721f6019a
OptimusJc <optimus.prime.jc@gmail.com>
2025-09-09 16:03:29 +0300

    add sitemap.xml

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,27 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+import sitemap from "vite-plugin-sitemap";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    sitemap({
+      hostname: "https://www.galgaloimports.coom",
+      dynamicRoutes: [
+        "/",
+        "/about-us",
+        "/product-overview",
+        "/contact-us",
+        "/import-process",
+      ],
+      changefreq: "weekly",
+      priority: 0.7,
+      generateRobotsTxt: true,
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
